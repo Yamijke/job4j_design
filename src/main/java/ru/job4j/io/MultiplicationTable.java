62fa2d1105c070010c6500863812f1dd43b33b3e
Yamijke <serhei.knyazev@gmail.com>
2023-07-27 11:41:56 +0300

    2.0. FileOutputStream [#252488  [#252488]]

---
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/multiplicationtable.txt")) {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    String output = i + " * " + j + " = " + (i * j);
+                    out.write(output.getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
