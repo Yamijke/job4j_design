031801d44abb3d656e32c8118af6b185a77ab146
Yamijke <serhei.knyazev@gmail.com>
2023-08-02 11:40:33 +0300

    10. Сканирование файловой системы [#106929  [#106929 #433940]]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -1,11 +1,14 @@
 package ru.job4j.io;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class SearchFiles extends SimpleFileVisitor<Path> {
+public class SearchFiles implements FileVisitor<Path> {
     private Predicate<Path> condition;
     private List<Path> paths;
 
@@ -14,11 +17,26 @@ public class SearchFiles extends SimpleFileVisitor<Path> {
     }
 
     @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
             paths.add(file);
         }
         return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return null;
     }
 
     public List<Path> getPaths() {
