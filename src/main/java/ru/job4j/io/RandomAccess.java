81f55f7a4d75457cb7918afc623b9fdb5d080844
Yamijke <serhei.knyazev@gmail.com>
2023-08-16 18:53:44 +0300

    17. Scanner [#504791 #437259]

---
+++ src/main/java/ru/job4j/io/RandomAccess.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccess {
+
+    public static void main(String[] args)  {
+        try {
+            RandomAccessFile randomAccess = new RandomAccessFile("data/random.txt", "rw");
+            randomAccess.writeInt(100);
+            randomAccess.writeChar('A');
+            randomAccess.writeBoolean(true);
+            randomAccess.seek(0);
+            System.out.println(randomAccess.readInt());
+            System.out.println(randomAccess.readChar());
+            System.out.println(randomAccess.readBoolean());
+            randomAccess.seek(4);
+            System.out.println(randomAccess.readChar());
+            System.out.println(randomAccess.getFilePointer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
