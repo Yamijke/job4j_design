4fda6d8352d524c776fbf0e755afee1832f0d7a7
Yamijke <serhei.knyazev@gmail.com>
2023-08-17 12:12:06 +0300

    17. Scanner [#504791 #437259]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -10,25 +10,21 @@ public class CSVReader {
         String[] filter = argsName.get("filter").split(",");
         List<String> names = new ArrayList<>();
         List<Integer> filterIndexes = new ArrayList<>();
-
         try (BufferedReader in = new BufferedReader(new FileReader(argsName.get("path")));
              PrintWriter print = !"stdout".equals(argsName.get("out"))
                      ? new PrintWriter(new FileWriter(argsName.get("out")))
                      : new PrintWriter(System.out)) {
-
             String first = in.readLine();
             if (first != null) {
                 String[] columns = first.split(argsName.get("delimiter"));
                 Collections.addAll(names, columns);
             }
-
             for (String name : filter) {
                 int index = names.indexOf(name);
                 if (index != -1) {
                     filterIndexes.add(index);
                 }
             }
-
             StringBuilder headerLine = new StringBuilder();
             for (String name : filter) {
                 headerLine.append(name).append(argsName.get("delimiter"));
@@ -37,29 +33,21 @@ public class CSVReader {
                 headerLine.setLength(headerLine.length() - 1);
             }
             print.println(headerLine);
-
             String line;
             while ((line = in.readLine()) != null) {
                 Scanner scanner = new Scanner(line).useDelimiter(argsName.get("delimiter"));
                 StringBuilder outputLine = new StringBuilder();
-
                 List<String> values = new ArrayList<>();
                 while (scanner.hasNext()) {
                     values.add(scanner.next());
                 }
-
                 for (int idx : filterIndexes) {
                     if (idx < values.size()) {
                         outputLine.append(values.get(idx)).append(argsName.get("delimiter"));
                     }
                 }
-
                 if (outputLine.length() > 0) {
                     outputLine.setLength(outputLine.length() - 1);
-                }
-
-                if ("stdout".equals(argsName.get("out"))) {
-                    System.out.println(outputLine);
                 }
                 print.println(outputLine);
                 scanner.close();
@@ -71,8 +59,8 @@ public class CSVReader {
 
     public static void validate(ArgsName argsName) {
         File file = new File(argsName.get("path"));
-        if (!Files.isDirectory(file.toPath())) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.toPath()));
+        if (!Files.isRegularFile(file.toPath())) {
+            throw new IllegalArgumentException(String.format("Not a file %s", file.toPath()));
         }
         String outFile = argsName.get("out");
         if (!("stdout".equals(outFile) || outFile.endsWith(".csv"))) {
