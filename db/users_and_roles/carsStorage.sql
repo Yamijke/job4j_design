72376153320dda31cf7bb8ee0930899a4ebaf76b
Yamijke <serhei.knyazev@gmail.com>
2023-09-04 14:29:48 +0300

    1. Хранилище машин [#1733]

--- db/users_and_roles/carsStorage.sql
+++ db/users_and_roles/carsStorage.sql
@@ -41,19 +41,19 @@ left join car_bodies cb on c.body_id = cb.id
 left join car_engines ce on c.engine_id = ce.id 
 left join car_transmissions ct on c.transmission_id = ct.id ;
 
---sql reauest where body not exists
+--sql request where body not exists
 select cb.*
 from car_bodies cb
 left join cars c on cb.id = c.body_id
 where c.id is null;
 
---sql reauest where engine not exists
+--sql request where engine not exists
 select ce.*
 from car_engines ce
 left join cars c on ce.id = c.engine_id 
 where c.engine_id is null;
 
---sql reauest where transmission not exists
+--sql request where transmission not exists
 select ct.*
 from car_transmissions ct
 left join cars c on ct.id = c.transmission_id
